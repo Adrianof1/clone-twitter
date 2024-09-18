cc79fa9a0405ee20bea7b3556c84f61d0d94f181
Adrianof1 <adrianoferreira8@outlook.com>
2024-09-17 23:47:03 -0300

    import icons

--- twitter/src/App.jsx
+++ twitter/src/App.jsx
@@ -1,9 +1,12 @@
+import { Siderbar } from "./components/Sidebar"
+
+
 function App() {
   return (
-    <div>
-      <h1>Twitter</h1>
+    <div className="flex mx-auto max-w-7xl">
+      <Siderbar />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ twitter/src/components/Sidebar/index.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export function Siderbar(){
+
+    return (
+        <div className="w-20 xl:w-64 sticky top-0 px-2 h-screen">
+            Sidebar
+        </div>
+    )
+}
