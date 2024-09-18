1cef2d1ebc4b6730569afcbccb81da3878916445
Adrianof1 <adrianoferreira8@outlook.com>
2024-09-17 23:24:38 -0300

    create index.jsx

--- twitter/src/App.jsx
+++ twitter/src/App.jsx
@@ -1,15 +1,9 @@
-
-
 function App() {
-
   return (
-    
-      <div>
-        <h1>Twitter</h1>
-      </div>
-        
-    
-  )
+    <div>
+      <h1>Twitter</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
